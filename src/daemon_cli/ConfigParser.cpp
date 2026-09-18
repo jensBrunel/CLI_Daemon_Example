1cd6657a696fdf0eb3f66e01c7303ed3e090c519
Jens Nelke <j.nelke@brunel.net>
2026-09-18 11:33:57 +0200

    Removed unused functions

--- src/daemon_cli/ConfigParser.cpp
+++ src/daemon_cli/ConfigParser.cpp
@@ -167,22 +167,3 @@ void ConfigParser::Load() {
     ParseValue(document, "");
 }
 
-std::string ConfigParser::GetValue(const std::string &strKey) const {
-    const auto it = m_mapValues.find(Uppercase(strKey));
-    if (it == m_mapValues.end()) {
-        return "";
-    }
-    return it->second;
-}
-
-std::string ConfigParser::GetValue(const std::string &arrayKey, const std::string &strKey) const {
-    const auto it = m_mapValues.find(Uppercase(arrayKey + "." + strKey));
-    if (it == m_mapValues.end()) {
-        return "";
-    }
-    return it->second;
-}
-
-bool ConfigParser::HasKey(const std::string &strKey) const {
-    return m_mapValues.find(Uppercase(strKey)) != m_mapValues.end();
-}
--- src/daemon_cli/ConfigParser.h
+++ src/daemon_cli/ConfigParser.h
@@ -27,26 +27,7 @@ public:
      */
     bool Open(const std::string &strPath);
 
-    /**
-     * @brief Get a configuration value by key.
-     * @param strKey Key name to look up.
-     * @return Value associated with the key or an empty string if missing.
-     */
-    std::string GetValue(const std::string &strKey) const;
 
-    /**
-     * @brief Get a configuration value by key from an array of values.
-     * @param strKey Key name to look up.
-     * @return Value associated with the key or an empty string if missing.
-     */
-    std::string GetValue(const std::string &arrayKey, const std::string &strKey) const;
-
-    /**
-     * @brief Check whether a key exists in the loaded config.
-     * @param strKey Key name to look up.
-     * @return true if the key is present, false otherwise.
-     */
-    bool HasKey(const std::string &strKey) const;
 
     /**
      * @brief Check whether a config file was successfully opened.
